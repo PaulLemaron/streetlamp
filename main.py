0e88033566657128f3ff36728c2cd206a193648a
PAUL LEMARON <55991207+PaulLemaron@users.noreply.github.com>
2024-07-07 21:42:05 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,20 @@
+def on_forever():
+    if input.light_level() < 150:
+        basic.show_leds("""
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            """)
+    elif input.light_level() < 90:
+        basic.show_leds("""
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            """)
+    else:
+        pass
+basic.forever(on_forever)
